bef8b2a25bafb7a57fab041fdfafa10ae6a6d29a
Martim Tiburcio <tiburciomartim@gmail.comm>
2025-11-19 09:27:51 -0300

    chalenge 05 - done

--- EX008___desafios/desafio004/result.php
+++ EX008___desafios/desafio004/result.php
@@ -12,11 +12,11 @@
     </header>
     <section>
         <?php //conversor de moedas v2.0, converter real para dolar, exibir a cotação do dollar.         
-            $inicio = date("m-d-Y", strtotime("- 7 days"));
+            $inicio = date("m-d-Y", strtotime("-7 days"));
             $fim = date("m-d-Y");
             $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $inicio .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
         
-            $dados = json_decode(file_get_contents($url), true);
+            $dados = json_decode(file_get_contents($url), true); //true return array | void return object
             $cotacao = $dados["value"][0]["cotacaoCompra"];
             $real = floatval($_GET["real"]);
             $dolar = $real / $cotacao; 
