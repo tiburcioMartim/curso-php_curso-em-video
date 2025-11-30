88ad717b1aaf36f88132fdf173b907c1c5c96502
Martim Tiburcio <tiburciomartim@gmail.comm>
2025-11-30 16:17:49 -0300

    conclusão desafio 009 - Médias Aritméticas

--- Desafios/009_Medias_Aritmeticas/index.php
+++ Desafios/009_Medias_Aritmeticas/index.php
@@ -8,27 +8,51 @@
 </head>
 
 <body>
-    <?php 
-        $numero = $_GET["numero"]??0;
-        $raiz_quadrada = number_format($numero ** (1/2), 3, ',', '.')??0;
-        $raiz_cubica = number_format($numero ** (1/3), 3, ',', '.')??0;
-        
+    <?php
+        $valor1 = $_GET["valor1"]??0;
+        $valor2 = $_GET["valor2"]??0;
+        $peso1 = $_GET["peso1"]??1;
+        $peso2 = $_GET["peso2"]??1;
+        $soma_pesos = $peso1 + $peso2;
+        $media_simples = ($valor1 + $valor2) / 2;
+
+        try {
+            if ($soma_pesos == 0) {
+                throw new DivisionByZeroError("A soma dos pesos zero não é possível, coloque um valor a partir de 1.");
+            }
+        } catch (DivisionByZeroError $e) {
+            $soma_pesos = 1;
+            echo "<script>alert('Erro no cálculo: " . $e->getMessage() . "');</script>";
+        }
+
+        $media_ponderada = (($valor1 * $peso1) + ($valor2 * $peso2)) / ($soma_pesos);
     ?>
+
     <main>
-        <h1>Informe um número</h1>
+        <h1>Médias Aritméticas</h1>
         <form action="<?php $_SERVER["PHP_SELF"]?>" method="get">
-            <label for="numero">Número</label>
-            <input type="number" name="numero" id="id-numero" placeholder="Digite um número.">
-            <input type="submit" value="Calcular Raízes">
+            <label for="valor1">1º Valor</label>
+            <input type="number" name="valor1" id="id-valor1" placeholder="Digite um valor." value="<?=$valor1?>" required>
+
+            <label for="peso1">1º Peso</label>
+            <input type="number" name="peso1" id="id-peso1" placeholder="Digite um peso." value="<?=$peso1?>" required>
+
+            <label for="valor2">2º Valor</label>
+            <input type="number" name="valor2" id="id-valor2" placeholder="Digite um valor." value="<?=$valor2?>" required>
+
+            <label for="peso2">2º Peso</label>
+            <input type="number" name="peso2" id="id-peso2" placeholder="Digite um peso." value="<?=$peso2?>" required>
+
+            <input type="submit" value="Calcular Médias">
         </form>
     </main>
 
     <section>
-        <h2>Resultado Final</h2>
-        <p>Analisando o <strong>número <?=$numero?></strong>, temos:</p>
+        <h2>Cálculo das Médias</h2>
+        <p>Analisando os valores <?=$valor1?> e <?=$valor2?>:</p>
         <ul>
-            <li>A sua raiz quadrada é <strong><?=$raiz_quadrada?></strong></li>
-            <li>A sua raiz cúbica é <strong><?=$raiz_cubica?></strong></li>
+            <li>A <strong>Média Aritmética Simples</strong> entre os valores é igual a <?=number_format($media_simples, 2, ',', '.')?>.</li>
+            <li>A <strong>Média Aritmética Ponderada</strong> com pesos <?=$peso1?> e <?=$peso2?> é igual a <?=number_format($media_ponderada, 2, ',', '.')?>.</li>
         </ul>
     </section>
 </body>
