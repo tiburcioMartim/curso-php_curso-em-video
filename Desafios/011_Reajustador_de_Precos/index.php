eeefea7a9074827b5a702f18e6458ccd57b76c34
Martim Tiburcio <tiburciomartim@gmail.comm>
2025-12-01 20:29:02 -0300

    Reajustador de Tempo

--- Desafios/011_Reajustador_de_Precos/index.php
+++ Desafios/011_Reajustador_de_Precos/index.php
@@ -3,17 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Título</title>
+    <title>Reajustador de preços</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+    <?php 
+        $valor_produto = $_POST["valor_produto"];
+        $percentual = $_POST["percentual"];
+        $valor_reajustado = $valor_produto * ($percentual / 100 + 1);
+
+        $padrao = numfmt_create('pt-br', NumberFormatter::CURRENCY);
+
+    ?>
+
     <main>
-        <h1>Título</h1>
+        <h1>Reajustador de preços</h1>
+        <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+            <label for="valor_produto">Preço do Produto (R$)</label>
+            <input type="number" name="valor_produto" id="id-valor_produto" value="<?=$valor_produto?>">
+
+            <label for="percentual">Qual será o percentual de reajuste? (<output id="range_valor"><?=$percentual?></output>%)</label>
+            <input type="range" name="percentual" id="id-percentual" min="0" max="100" value="<?=$percentual?>">
+
+            <input type="submit" value="Reajustar">
+        </form>
     </main>
 
     <section>
-        <h2>Título</h2>
+        <h2>Resultado do Reajuste</h2>
+        <p>O produto que custava <?=numfmt_format_currency($padrao, $valor_produto, "BRL")?> com <strong><?=$percentual?>% de aumento</strong> vai passar a custar <strong><?=numfmt_format_currency($padrao, $valor_reajustado, "BRL")?></strong> a partir de agora.</p>
     </section>
+
+    <!-- Porcentagem dinamica -->
+    <script>
+        const rangeInput = document.getElementById('id-percentual');
+        const rangeValor = document.getElementById('range_valor');
+
+        rangeInput.addEventListener('input', () => {
+            rangeValor.textContent = rangeInput.value;
+        });
+    </script>
 </body>
 </html>
--- Desafios/012_Calculadora_de_Tempo/index.php
+++ Desafios/012_Calculadora_de_Tempo/index.php
@@ -3,17 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Título</title>
+    <title>Calculadora de Tempo</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <main>
-        <h1>Título</h1>
+        <h1>Calculadora de Tempo</h1>        
+        <form action="<?php $_SERVER["PHP_SELF"]?>" method="get">
+            <label for="">Qual é o total de segundos?</label>
+            <input type="number" name="" id="">
+            <input type="submit" value="Calcular">
+        </form>
     </main>
 
     <section>
-        <h2>Título</h2>
+        <h2>Totalizando tudo</h2>
+        <p>Analisando o valor que você digitou, 450 segundos equivalem a um total de:</p>
+        <ul>
+            <li>0 semanas</li>
+            <li>0 dias</li>
+            <li>0 horas</li>
+            <li>0 minutos</li>
+            <li>0 segundos</li>
+        </ul>
     </section>
 </body>
 </html>
